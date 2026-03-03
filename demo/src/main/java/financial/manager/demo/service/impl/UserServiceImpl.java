31896f50f744a5d80c99d372e70ff4e905082d3b
CauaGNP <163431893+CauaGNP@users.noreply.github.com>
2026-03-03 10:56:29 +0000

    feat: Add 2 services function

--- demo/src/main/java/financial/manager/demo/service/impl/UserServiceImpl.java
+++ demo/src/main/java/financial/manager/demo/service/impl/UserServiceImpl.java
@@ -1,7 +1,38 @@
 package financial.manager.demo.service.impl;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.stereotype.Service;
+
+import financial.manager.demo.DTO.request.user.UserRequestBasicDTO;
+import financial.manager.demo.DTO.response.user.UserResponseSummaryDTO;
+import financial.manager.demo.repository.UserRepository;
 import financial.manager.demo.service.UserService;
 
+@Service
 public class UserServiceImpl implements UserService {
 
+    private final UserRepository userRepository;
+
+    UserServiceImpl(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public List<UserResponseSummaryDTO> getAllUsers() {
+
+        return userRepository.findAll()
+                .stream()
+                .map(user -> new UserResponseSummaryDTO(user.getUserName(), user.getEmail()))
+                .toList();
+    }
+
+    @Override 
+    public UserResponseSummaryDTO getUserById(UserRequestBasicDTO request){
+        return userRepository.findById(request.getId())
+                .map(user -> new UserResponseSummaryDTO(user.getUserName(), user.getEmail()))
+                .orElseThrow(() -> new RuntimeException("User not Found"));
+                
+    }
 }
